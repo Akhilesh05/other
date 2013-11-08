8dcf66d591ff9be00a375658b34239fe47a126e5
Girish Gopaul <akhile@dr.com>
2013-11-08 19:04:47 +0000

    Update Scala_OOP.scala

--- Scala_OOP.scala
+++ Scala_OOP.scala
@@ -14,37 +14,13 @@ class Quadratic(aa:Double, bb:Double, cc:Double){
 }
 
 object Main {
-	var x:Quadratic = new Quadratic(3,13,6)
-	var y:List[Double] = (new Quadratic(2,8,4)).solve()
-	println(y)
-	println(x.a)
-	println(x.solve)
-	
-   def addInt(a:Int)(b:Int) : Int = {
-   	a + b
-   }
-   def mult(a:Int, b:Int):Int = {
-   	a * b
-   }
+
    def main(args: Array[String]) {
-      println(addInt(5){
-          var x : Int = 3
-          var y : Int = 2
-          x + y
-        }
-      )
-      println(mult(3,{
-      	var x:Int = 2
-      	var y:Int = 4
-      	x * y
-      }))
-      var x:Map[String, String] = Map("Akh" -> "Best", "Udaye" -> "Good")
-      println(x("Akh"))
-      x.keys.foreach{ a =>
-          println(a)
-      }
-      for(a <- x.values){
-      	println(a)
-      }
+   	    var x:Quadratic = new Quadratic(3,13,6)
+        var y:List[Double] = (new Quadratic(2,8,4)).solve()
+        println(y)
+        println(x.a)
+        println(x.solve)
    }
+   
 }

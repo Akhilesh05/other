def003297d610f724abf2fe3a6aa9c23db542b2a
Girish Gopaul <akhile@dr.com>
2013-11-08 19:55:02 +0000

    Update Scala_OOP.scala

--- Scala_OOP.scala
+++ Scala_OOP.scala
@@ -13,16 +13,20 @@ class QuadraticEqt(aa:Double=0, bb:Double=0, cc:Double=0){
 		2*a*x + b
 	}
 	
+	def normalAt(x:Double):Double = {
+		-1 / gradAt(x)
+	}
+	
 	def solve():List[Double] = {
 		var x1 = (-b+ sqrt(pow(b,2) - 4*a*c))/(2*a)
 		var x2 = (-b- sqrt(pow(b,2) - 4*a*c))/(2*a)
 		List(x1, x2)
 	}
 	
-	def stationary():List[Double] = {
+	def stationary():Map[String, Double] = {
 		var x:Double = -b/(2*a)
 		var y:Double = -pow(b,2)/(4*a) + c
-		List(x, y)
+		Map("x" -> x, "y" -> y)
 	}
 	
 }
@@ -31,8 +35,11 @@ object Main {
 
    def main(args: Array[String]) {
    	    var x:QuadraticEqt = new QuadraticEqt(3,13,6)
-   	    //var y:List[Double] = (new QuadraticEqt(2,8,4)).solve()
-   	    println(x.gradAt(x.stationary()(0)))  //equals zero everytime
+   	    println(x.gradAt(x.stationary()("x")))  //equals zero everytime
+   	    var y = (new QuadraticEqt(2,4,6)).stationary
+   	    println(y)
+   	    println(x.gradAt(3))
+   	    println(x.normalAt(3))
    }
    
 }

c33d140a5e0f0d816f1faef70ca230bce418b687
Girish Gopaul <akhile@dr.com>
2013-11-08 19:33:29 +0000

    Update Scala_OOP.scala

--- Scala_OOP.scala
+++ Scala_OOP.scala
@@ -1,9 +1,17 @@
 import scala.math._
 
-class Quadratic(aa:Double, bb:Double, cc:Double){
+class QuadraticEqt(aa:Double=0, bb:Double=0, cc:Double=0){
 	var a:Double = aa
 	var b:Double = bb
 	var c:Double = cc
+	
+	def yIntercept():Double = {
+		c
+	}
+	
+	def gradAt(x:Double):Double = {
+		2*a*x + b
+	}
 	
 	def solve():List[Double] = {
 		var x1 = (-b+ sqrt(pow(b,2) - 4*a*c))/(2*a)
@@ -11,16 +19,20 @@ class Quadratic(aa:Double, bb:Double, cc:Double){
 		List(x1, x2)
 	}
 	
+	def stationary():List[Double] = {
+		var x:Double = -b/(2*a)
+		var y:Double = -pow(b,2)/(4*a) + c
+		List(x, y)
+	}
+	
 }
 
 object Main {
 
    def main(args: Array[String]) {
-   	    var x:Quadratic = new Quadratic(3,13,6)
-   	    var y:List[Double] = (new Quadratic(2,8,4)).solve()
-   	    println(y)
-   	    println(x.a)
-   	    println(x.solve)
+   	    var x:QuadraticEqt = new QuadraticEqt(3,13,6)
+   	    //var y:List[Double] = (new QuadraticEqt(2,8,4)).solve()
+   	    println(x.gradAt(x.stationary()(0)))  //equals zero everytime
    }
    
 }
